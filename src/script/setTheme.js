35634204393e156a27dd2d85d9833c070c10840c
cyril-fl <cyril-f@hotmail.com>
2024-04-20 10:28:20 +0200

    * try to implement changing theme * on a good way

--- src/script/setTheme.js
+++ src/script/setTheme.js
@@ -1,24 +1,10 @@
-function addClassToElements(className) {
-    // Get all HTML elements
-    var elements = document.getElementsByTagName('*');
-
-    // Loop through all elements
-    for (var i = 0; i < elements.length; i++) {
-        // Add the class to each element
-        elements[i].classList.add(className);
-    }
-}
-
 const prefersMode = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
 
 const setDark = () => {
     document.body.setAttribute('data-theme', 'dark')
-    addClassToElements('dark-theme');
 }
 const setLight = () => {
     document.body.setAttribute('data-theme', 'light');
-    addClassToElements('light-theme');
-
 }
 
 
