bada4cbd7fb10a3bdc4c7e51debd348c0c77ba35
cyril-fl <cyril-f@hotmail.com>
2024-04-23 12:32:20 +0200

    * Calendar on its way

--- src/component/CalendarView.jsx
+++ src/component/CalendarView.jsx
@@ -7,7 +7,31 @@ const CalendarView = () => {
         let initMonth = thisCalendar.getMonthName(initDate)
 
 
-    // console.log(thisCalendar)
+    const generateCalendarRows = () => {
+        const calendarRows = [];
+        let day = 1;
+
+        // Boucle pour générer les lignes du calendrier
+        for (let i = 0; i < 6; i++) {
+            const rowCells = [];
+
+            // Boucle pour générer les cellules de chaque ligne
+            for (let j = 0; j < 7; j++) {
+                if (i === 0 && j < firstDayIndex) {
+                    rowCells.push(<td key={`empty-${j}`}></td>); // Utilisation de la méthode push() pour ajouter les cellules à la ligne
+                } else if (day <= daysInMonth) {
+                    rowCells.push(<td key={`day-${day}`} className="dayNum">{day}</td>);
+                    day++;
+                }
+            }
+
+            // Création de la ligne de calendrier
+            calendarRows.push(<tr key={`row-${i}`} className="calendarRaw">{rowCells}</tr>);
+        }
+
+        return calendarRows;
+    }
+
 
     return (
         <>
@@ -23,10 +47,12 @@ const CalendarView = () => {
                             <th key={dayName} className={'dayName'} >{ dayName.slice(0, 3) }</th>
                         ))}
                     </tr>
-
-
-                    {/*<tr><th>Lun</th><th>Mar</th><th>Mer</th><th>Jeu</th><th>Ven</th><th>Sam</th><th>Dim</th></tr>*/}
                     </thead>
+                    <tbody>
+                    {
+                        generateCalendarRows()
+                    }
+                    </tbody>
 
                 </table>
 
